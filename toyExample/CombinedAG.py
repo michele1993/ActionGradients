Getting correct variance values for Izawa & Shadmer 2011

toyExample/CombinedAG.py
@@ -2,13 +2,16 @@ import torch
 
 class CombActionGradient:
 
-    def __init__(self, actor, beta_mu, beta_std):
+    def __init__(self, actor, beta_mu, beta_std, rbl_std_weight=1, ebl_std_weight=1):
 
         assert beta_mu >= 0 and beta_mu <= 1, "beta must be between 0 and 1 (inclusive)"
 
         self.actor = actor
         self.beta_mu = beta_mu
         self.beta_std = beta_std
+
+        self.rbl_std_weight = rbl_std_weight
+        self.ebl_std_weight = ebl_std_weight
 
         self.beta = torch.tensor([beta_mu, beta_std])
     
@@ -33,7 +36,7 @@ class CombActionGradient:
             R_dr_dmu_a = (1/(std_a**2)) * (action - mu_a) * delta_rwd
             # Std action grad
             #R_dr_dstd_a = (delta_rwd * ((action - mu_a)**2 - std_a**2) / std_a**3)
-            R_dr_dstd_a = 15*(delta_rwd * (action - mu_a)**2)
+            R_dr_dstd_a = self.rbl_std_weight * (delta_rwd * (action - mu_a)**2)
 
         #Combine two grads relative to mu and std into one vector
         R_grad = torch.cat([R_dr_dmu_a, R_dr_dstd_a])
@@ -51,7 +54,7 @@ class CombActionGradient:
         ## NOTE: During error-based learning variance seems fixed!!! so not plausible the variance reduction
         #Error-based learning will try to converge to deterministic policy
         std_loss = std_a**2
-        E_dr_dstd_a =  torch.autograd.grad(std_loss, std_a, retain_graph=True)[0]
+        E_dr_dstd_a =  self.ebl_std_weight * torch.autograd.grad(std_loss, std_a, retain_graph=True)[0]
 
         # TRIAL: Error-based learning not controlling std
         #E_dr_dstd_a = torch.zeros_like(E_dr_dmu_a)